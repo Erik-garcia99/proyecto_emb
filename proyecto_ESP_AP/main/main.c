ced0a8cad62ab40f55b57b0765296f5daff8f5ae
dmrx <dmrx@gmail.com>
2025-11-23 22:58:11 -0800

    iniciando comunicacion ESP_AP

--- proyecto_ESP_AP/main/main.c
+++ proyecto_ESP_AP/main/main.c
@@ -1,6 +1,122 @@
+//librerias estandars 
 #include <stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+//librerias de freertos 
+#include<freertos/FreeRTOS.h>
+#include<freertos/task.h>
+#include<freertos/queue.h>
+
+
+//libereias de manejo de errores 
+#include<esp_log.h>
+#include<esp_err.h>
+
+//drivers 
+#include<driver/uart.h>
+
+//librerias propias 
+#include"modulos/UART/uart_lib.h"
+
+//macros 
+//recibe desde UART1 - igual lo cambiamos es lo verga 
+#define NUM_UART UART_NUM_1
+#define TX_PIN 19
+#define RX_PIN 18
+
+
+//varibales 
+extern QueueHandle_t event_uart; //cola que recibe los datos de UART. 
+QueueHandle_t send_temp;
+
+static const char *TAG = "ESP_AP";
+
+
+//funciones 
+
+//tareas 
+
+
 
 void app_main(void)
 {
+
+    //inciando colas 
+    send_temp = xQueueCreate(10, sizeof(char[12])); /// la cola por la cual enviaremos o que se reciba del ESP que tiene el sensor de tmeperatura 
+
+
+
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    //seccion de UART 
+
+    init_uart(NUM_UART,TX_PIN,RX_PIN,UART_DATA_8_BITS,UART_PARITY_DISABLE,UART_STOP_BITS_1);
+
+
+    //inicamos la tarea la cual se encarga de estar esperando a que se reciba lago por UART. 
+    //debe de ener un numero de prioridad alto porque es lo que estara mostrando dentro de la pagina. 
+    xTaskCreate(uart_task, "uart_task", 4098, NULL, 9, NULL);
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+}
+
+
+void uart_task(void *params){
+
+    uart_event_t event; 
+    uint8_t *buffer=(uint8_t*)malloc(BUFF);
+    char data[12]={0};
+    while(1){
+        if(xQueueReceive(event_uart, (void *)&event, portMAX_DELAY)){
+            memset(buffer,0, BUFF);
+
+            switch(event.type){
+
+                case UART_DATA:{
+                    //recibio alg mi pa 
+
+                    int len; 
+
+                    len = uart_read_bytes(NUM_UART, buffer,event.size, portMAX_DELAY);
+                    ESP_LOGI(TAG,"len: %d DATA:",event.size);
+                    uart_write_bytes(NUM_UART, (const char *)buffer, event.size);
+                    uart_write_bytes(NUM_UART, "\n", 2);
+
+                    memset(data, 0, sizeof(data));
+                    strncpy(data, (char*)buffer, sizeof(data) - 1);
+    
+                    
+
+                    //una vez recibida la infromacion no como enviamos la infroacion para que sea procesada en la pagina web?, podemos enviar   la infromacion por una cola 
+                    xQueueSend(send_temp, data, 0);
+
+                }break;
+
+                case UART_BUFFER_FULL:{
+                    ESP_LOGI(TAG,"BUFFER FULL");
+                    uart_flush_input(NUM_UART);
+                    xQueueReset(event_uart);
+                }break;
+
+                case UART_FIFO_OVF:{
+                    ESP_LOGI(TAG, "FIFO OVERFLOW");
+                    uart_flush_input(NUM_UART);
+                    xQueueReset(event_uart);
+                }break;
+
+                default :{
+                    ESP_LOGI(TAG, "uart event type %s", event.type);
+                }
+
+
+
+            }
+
+
+
+        }
+    }
+
+
 
 }
---
+++ proyecto_ESP_AP/main/modulos/UART/uart_lib.h
@@ -0,0 +1,50 @@
+#ifndef UART_LIB_H
+#define UART_LIB_H
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/queue.h>
+#include <stdint.h>
+#include <esp_err.h>
+#include <driver/uart.h>
+//macros
+#define BUFF 1024
+
+//varibales
+extern QueueHandle_t event_uart;
+
+
+//definiciones de funciones 
+
+
+/**
+ * 
+ * @brief define el UART, todos sus parametros 
+ * 
+ * @param num_uart el numero del uart 
+ * @param pin_tx pin del transmisor
+ * @param pin_rx pin del reseptor 
+ * @param data el tamanio de bits del frame
+ * @param bits_parity habilitado o deshabitiadp el bit de paridad
+ * @param stop_bits_f la cantidad de stop bits que delimietan el final del frame
+ * 
+ * @return
+ *  ESP_OK -> si la configuracion se realizo con exito 
+ *  ESP_FAIL -> si ocurrio un error que algun parametro no esta bien 
+ * 
+ */
+esp_err_t init_uart(uart_port_t num_uart,int pin_tx, int pin_rx, uart_word_length_t data,uart_parity_t bits_parity,uart_stop_bits_t stop_bits_f);
+
+
+//tareas 
+/**
+ * @brief tarea que se encargara de recibir y procesar los datos obtenidos por UART 
+ * 
+ * 
+ * 
+ * 
+ * @return -> noting
+ */
+
+void uart_task(void *params);
+
+#endif
